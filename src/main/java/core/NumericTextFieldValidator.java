bc8a586a2fd5535f2f6cd302a0279d7bb7a38541
reedfenters <rfenters95@gmail.com>
2018-02-17 14:30:02 -0500

    Added alerts to textField input validators.

--- src/main/java/core/NumericTextFieldValidator.java
+++ src/main/java/core/NumericTextFieldValidator.java
@@ -2,6 +2,7 @@ package core;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import org.apache.log4j.Logger;
 
@@ -32,45 +33,92 @@ public class NumericTextFieldValidator implements ChangeListener<String> {
     @Override
     public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 
-        // check if newValue is num
-        if (newValue.matches("-|0-|(-\\d|\\d)\\d*")) {
-            LOGGER.trace(String.format("newValue [%s] has matched pattern!", newValue));
-            if (!newValue.matches("-|0-|-0")) {
-                int value = Integer.parseInt(newValue);
-                if (newValue.matches("0+\\d+")) {
-                    LOGGER.trace(String.format("newValue [%s] rejected! Value contains leading zeros.", newValue));
-                    textField.setText(String.valueOf(value));
-                } else if (newValue.matches("-(0+)(\\d+)") && shouldPreventNegatives) {
-                    textField.setText(oldValue);
-                } else if (newValue.matches("-(0+)(\\d+)") && !shouldPreventNegatives) {
-                    value = Integer.parseInt(newValue.substring(1));
-                    value *= -1;
-                    textField.setText(String.valueOf(value));
-                } else {
-                    // check if newValue is valid num
-                    if ((value < lowerLimit || value > upperLimit) && !specialValues.contains(value)) {
-                        LOGGER.trace(String.format("newValue [%s] rejected! Value exceeds limits.", newValue));
-                        textField.setText(oldValue);
-                    } else {
-                        LOGGER.trace(String.format("newValue [%s] accepted!", newValue));
-                    }
-                }
-            } else {
-                if (newValue.matches("0-|-0") && !shouldPreventNegatives) {
-                    LOGGER.trace(String.format("newValue [%s] simplified to [-]!", newValue));
-                    textField.setText("-");
-                } else if (newValue.matches("-|0-|-0") && shouldPreventNegatives) {
-                    LOGGER.trace("[-] symbol prevented! lowerLimit is nonNegative!");
-                    textField.setText(oldValue);
-                }
-            }
-        } else if (newValue.isEmpty()) {
-            LOGGER.trace("newValue is empty! TextField set to zero.");
-            textField.setText("0");
-        } else {
-            LOGGER.trace(String.format("newValue [%s] rejected! Value does not match pattern.", newValue));
+        if (!isValueNumeric(newValue)) {
             textField.setText(oldValue);
+            return;
         }
+
+        if (newValue.isEmpty()) {
+            textField.setText("0");
+            return;
+        }
+
+        if (!hasNumericFragment(newValue)) {
+
+            int value = Integer.parseInt(newValue);
+
+            if (hasLeadingZeros(newValue)) {
+                textField.setText(String.valueOf(value));
+                return;
+            }
+
+            if (isNegative(value) && shouldPreventNegatives) {
+                textField.setText(oldValue);
+                return;
+            }
+
+            if (hasZeroLayer(newValue) && isNegative(value)) {
+                textField.setText(String.valueOf(value));
+                return;
+            }
+
+            if (isOutOfBounds(value) && !isSpecialValue(value)) {
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+                alert.setHeaderText("Invalid input");
+
+                String message =
+                        String.format("Value: %s exceeds the bounds for this input field!\n", value) +
+                                String.format("Upper bound: %s\n", upperLimit) +
+                                String.format("Lower bound: %s", lowerLimit);
+                alert.setContentText(message);
+
+                alert.showAndWait();
+
+                textField.setText(oldValue);
+                return;
+            }
+
+        } else {
+            if (shouldPreventNegatives) {
+                textField.setText(oldValue);
+                return;
+            }
+
+            if (newValue.matches("0-|-0") && !shouldPreventNegatives) {
+                textField.setText("-");
+                return;
+            }
+        }
+    }
+
+    private boolean isValueNumeric(String value) {
+        return value.matches("-|0-|(-\\d|\\d)\\d*");
+    }
+
+    private boolean hasNumericFragment(String value) {
+        return value.matches("-|0-|-0");
+    }
+
+    private boolean hasLeadingZeros(String value) {
+        return value.matches("0+\\d+");
+    }
+
+    private boolean hasZeroLayer(String value) {
+        return value.matches("-(0+)(\\d+)");
+    }
+
+    private boolean isNegative(int value) {
+        return value < 0;
+    }
+
+    private boolean isOutOfBounds(int value) {
+        return value < lowerLimit || value > upperLimit;
+    }
+
+    private boolean isSpecialValue(int value) {
+        return specialValues.contains(value);
     }
 
 }
